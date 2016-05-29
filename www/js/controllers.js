f53eb40062957d6ad11618c14622cc9c3f368202
daniloluca <daniloluca@live.com>
2016-05-28 21:35:50 -0300

    Indentação.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,83 +1,83 @@
 angular.module('starter.controllers', [])
 
 .controller('PodcastsCtrl', function($scope, $ionicPopup, $state, Podcasts) {
-  $scope.search = function(){
-    $scope.data = {};
-    $ionicPopup.show({
-      template: '<input type="Text" ng-model="data.term" autofocus>',
-      title: 'Search',
-      scope: $scope,
-      buttons: [
-        { text: 'Cancel' },
-        {
-          text: '<b>Search</b>',
-          type: 'button-positive',
-          onTap: function(e) {3
-            $state.go('tab.podcasts-search', {term: $scope.data.term})
-          }
-        },
-      ]
-    });
-  };
+    $scope.search = function(){
+        $scope.data = {};
+        $ionicPopup.show({
+            template: '<input type="Text" ng-model="data.term" autofocus>',
+            title: 'Search',
+            scope: $scope,
+            buttons: [
+                { text: 'Cancel' },
+                {
+                    text: '<b>Search</b>',
+                    type: 'button-positive',
+                    onTap: function(e) {3
+                        $state.go('tab.podcasts-search', {term: $scope.data.term})
+                    }
+                },
+            ]
+        });
+    };
 
-  $scope.podcasts = Podcasts.allPodcasts();
+    $scope.podcasts = Podcasts.allPodcasts();
 })
 
 .controller('SearchCtrl', function($scope, $stateParams, $ionicLoading, $ionicListDelegate, Podcasts){
-  $ionicLoading.show({template: 'Loading...'});
+    $ionicLoading.show({template: 'Loading...'});
 
-  Podcasts.searchPodcast($stateParams.term).then(function(response){
-    var podcasts = Podcasts.allPodcasts();
-    for(var i=0; i<response.data.length; i++){
-      response.data[i].subscribed = false;
-      for(var j=0; j<podcasts.length; j++){
-        if(response.data[i].username == podcasts[j].username){
-          response.data[i].subscribed = true;
-          break;
+    Podcasts.searchPodcast($stateParams.term).then(function(response){
+        var podcasts = Podcasts.allPodcasts();
+        for(var i=0; i<response.data.length; i++){
+            response.data[i].subscribed = false;
+            for(var j=0; j<podcasts.length; j++){
+                if(response.data[i].username == podcasts[j].username){
+                    response.data[i].subscribed = true;
+                    break;
+                }
+            }
         }
-      }
+        $scope.podcasts = response.data;
+        $ionicLoading.hide();
+    });
+
+    $scope.subscribe = function(podcast){
+        podcast.subscribed = true;
+        Podcasts.subscribe(podcast);
+        $ionicListDelegate.closeOptionButtons();
     }
-    $scope.podcasts = response.data;
-    $ionicLoading.hide();
-  });
 
-  $scope.subscribe = function(podcast){
-    podcast.subscribed = true;
-    Podcasts.subscribe(podcast);
-    $ionicListDelegate.closeOptionButtons();
-  }
-
-  $scope.unsubscribe = function(podcast){
-    Podcasts.unsubscribe(podcast);
-    $ionicListDelegate.closeOptionButtons();
-  }
+    $scope.unsubscribe = function(podcast){
+        Podcasts.unsubscribe(podcast);
+        $ionicListDelegate.closeOptionButtons();
+    }
 })
 
 .controller('PodcastEpisodesCtrl', function($scope, $stateParams, $http, $ionicLoading, Podcasts) {
-  $scope.podcastId = $stateParams.podcastId;
+    $scope.podcastId = $stateParams.podcastId;
 
-  $ionicLoading.show({template: 'Loading...'});
+    $ionicLoading.show({template: 'Loading...'});
 
-  Podcasts.getEpisodes($stateParams.podcastId).then(function(response){
-    $scope.episodes = response.data;
-    $ionicLoading.hide();
-  });
+    Podcasts.getEpisodes($stateParams.podcastId).then(function(response){
+        $scope.episodes = response.data;
+        $ionicLoading.hide();
+    });
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
+    $scope.chats = Chats.all();
 
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+    $scope.remove = function(chat) {
+        Chats.remove(chat);
+    };
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+    $scope.chat = Chats.get($stateParams.chatId);
 })
 
 .controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+    $scope.settings = {
+        enableFriends: true
+    };
 });
--- www/js/services.js
+++ www/js/services.js
@@ -2,110 +2,110 @@ angular.module('starter.services', [])
 
 .factory('Chats', function() {
 
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
+    var chats = [{
+        id: 0,
+        name: 'Ben Sparrow',
+        lastText: 'You on your way?',
+        face: 'img/ben.png'
+    }, {
+        id: 1,
+        name: 'Max Lynx',
+        lastText: 'Hey, it\'s me',
+        face: 'img/max.png'
+    }, {
+        id: 2,
+        name: 'Adam Bradleyson',
+        lastText: 'I should buy a boat',
+        face: 'img/adam.jpg'
+    }, {
+        id: 3,
+        name: 'Perry Governor',
+        lastText: 'Look at my mukluks!',
+        face: 'img/perry.png'
+    }, {
+        id: 4,
+        name: 'Mike Harrington',
+        lastText: 'This is wicked good ice cream.',
+        face: 'img/mike.png'
+    }];
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    return {
+        all: function() {
+            return chats;
+        },
+        remove: function(chat) {
+            chats.splice(chats.indexOf(chat), 1);
+        },
+        get: function(chatId) {
+            for (var i = 0; i < chats.length; i++) {
+                if (chats[i].id === parseInt(chatId)) {
+                    return chats[i];
+                }
+            }
+            return null;
         }
-      }
-      return null;
-    }
-  };
+    };
 })
 
 .factory("Podcasts", function($http){
 
-  var podcasts = JSON.parse(window.localStorage.getItem("podcasts")) || [];
+    var podcasts = JSON.parse(window.localStorage.getItem("podcasts")) || [];
 
-  return {
-    allPodcasts: function(){
-      return podcasts;
-    },
-    unsubscribe: function(podcast){
-      // podcasts.splice(podcasts.indexOf(podcast), 1);
+    return {
+        allPodcasts: function(){
+            return podcasts;
+        },
+        unsubscribe: function(podcast){
+            // podcasts.splice(podcasts.indexOf(podcast), 1);
 
-      podcasts.splice(podcasts.indexOf(podcast), 1);
-      window.localStorage.setItem("podcasts", JSON.stringify(podcasts));
-    },
-    subscribe: function(podcast){
-      // podcast.id = podcasts.length;
-      // podcasts.push(podcast);
+            podcasts.splice(podcasts.indexOf(podcast), 1);
+            window.localStorage.setItem("podcasts", JSON.stringify(podcasts));
+        },
+        subscribe: function(podcast){
+            // podcast.id = podcasts.length;
+            // podcasts.push(podcast);
 
-      podcast.id = podcasts.length;
-      podcasts.push(podcast);
-      window.localStorage.setItem("podcasts", JSON.stringify(podcasts));
-    },
-    searchPodcast: function(term){
-      var promise;
+            podcast.id = podcasts.length;
+            podcasts.push(podcast);
+            window.localStorage.setItem("podcasts", JSON.stringify(podcasts));
+        },
+        searchPodcast: function(term){
+            var promise;
 
-      if (!promise) {
-        promise = $http.get("https://api.mixcloud.com/search/?q="+ term.replace(' ', '+') +"&type=user").then(function (response) {          
-          return response.data;
-        });
-      }
+            if (!promise) {
+                promise = $http.get("https://api.mixcloud.com/search/?q="+ term.replace(' ', '+') +"&type=user").then(function (response) {
+                    return response.data;
+                });
+            }
 
-      return promise;
-    },
-    getPodcast: function(id){
-      for(var i=0; i<podcasts.length; i++){
-        if(podcasts[i].id == id){
-          return podcasts[i];
+            return promise;
+        },
+        getPodcast: function(id){
+            for(var i=0; i<podcasts.length; i++){
+                if(podcasts[i].id == id){
+                    return podcasts[i];
+                }
+            }
+        },
+        getEpisodes: function(id){
+            var username;
+            console.log(podcasts);
+            for(var i=0; i<podcasts.length; i++){
+                if(podcasts[i].id == id){
+                    username = podcasts[i].username;
+                    break;
+                }
+            }
+
+            var promise;
+
+            if (!promise) {
+                promise = $http.get("https://api.mixcloud.com/"+ username +"/cloudcasts/").then(function (response) {
+                    return response.data;
+                });
+            }
+
+            return promise;
         }
-      }
-    },
-    getEpisodes: function(id){
-      var username;
-      console.log(podcasts);
-      for(var i=0; i<podcasts.length; i++){
-        if(podcasts[i].id == id){
-          username = podcasts[i].username;
-          break;
-        }
-      }
-
-      var promise;
-
-      if (!promise) {
-        promise = $http.get("https://api.mixcloud.com/"+ username +"/cloudcasts/").then(function (response) {          
-          return response.data;
-        });
-      }
-
-      return promise;
-    }
-  };
+    };
 });
